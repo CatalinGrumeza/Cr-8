8b55196297798aace23a46cac226efef61735f65
Catalin Grumeza <catalin.grumeza@edu.itspiemonte.it>
2024-11-29 15:23:55 +0100

    Prj Created

---
+++ Cr-8/src/main/java/com/Cr_8/Cr8Application.java
@@ -0,0 +1,13 @@
+package com.Cr_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cr8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cr8Application.class, args);
+	}
+
+}
