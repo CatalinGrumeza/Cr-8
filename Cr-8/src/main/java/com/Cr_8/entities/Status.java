0ce58df97c929caf1d1c60f06179155f2079d1ad
ggiaco-lab <alessio.giacometti@edu.itspiemonte.it>
2024-11-30 15:34:57 +0100

    Created status entity

---
+++ Cr-8/src/main/java/com/Cr_8/entities/Status.java
@@ -0,0 +1,37 @@
+package com.Cr_8.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * Entity class representing the status of the requests in the database.
+ */
+@Entity
+@Table(name = "status")
+public class Status {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
