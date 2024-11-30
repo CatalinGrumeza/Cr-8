d666af0dbe9d58bf5555fd530678ac01d52a361d
Catalin Grumeza <catalin.grumeza@edu.itspiemonte.it>
2024-11-30 17:12:21 +0100

    Created email sender and added it to info post

--- Cr-8/src/main/java/com/Cr_8/controllers/InfoController.java
+++ Cr-8/src/main/java/com/Cr_8/controllers/InfoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.Cr_8.entities.Info;
 import com.Cr_8.servicies.InfoService;
+import com.Cr_8.servicies.MailService;
 
 
 
@@ -19,6 +20,10 @@ public class InfoController {
 	
 	@Autowired
 	private InfoService infoService;
+	@Autowired
+	private MailService mailService;
+	
+	
 	@GetMapping("/api/index")
 	public ResponseEntity<List<Info>> getAll() {
 		List<Info> infoList=infoService.getAllInfo();
@@ -27,6 +32,8 @@ public class InfoController {
 	@PostMapping("/api/createInfo")
 	public ResponseEntity<String> postMethodName(@RequestBody Info info) {
 		infoService.createInfo(info);
+		mailService.sendEmail(info.getEmail(), info.getText(),"Richiesta informazioni",info.getName(),info.getSurname(),"informazioni");
+		mailService.sendEmailToAdmin(info.getEmail(), info.getText(),"Richiesta informazioni",info.getName(),info.getSurname());
 		return ResponseEntity.ok("Info created successfully!");
 	}
 	
