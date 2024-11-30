0c682dc0886ecade886a5f14786d6b762874ed98
ggiaco-lab <alessio.giacometti@edu.itspiemonte.it>
2024-11-30 17:14:25 +0100

    Created reference repo (#12)

---
+++ Cr-8/src/main/java/com/Cr_8/repositories/ReferenceRepo.java
@@ -0,0 +1,19 @@
+package com.Cr_8.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Cr_8.entities.Reference;
+
+public interface ReferenceRepo extends JpaRepository<Reference,Integer> {
+
+	/**
+     * Finds a reference by its id.
+     *
+     * @param id the id of the reference to be found
+     * @return an Optional containing the Reference object if found, or an empty Optional if not found
+     */
+	Optional<Reference> findById(Integer id);
+	
+}
