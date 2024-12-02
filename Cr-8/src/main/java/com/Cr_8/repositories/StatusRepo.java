455a4fd45c02801d35b24212a8a4cbf6d3276d43
musty200 <mustafa.muhagr@edu.itspiemonte.it>
2024-12-02 14:27:27 +0100

    modify::typo id from integer to int and added service class of book klass (#25)

--- Cr-8/src/main/java/com/Cr_8/repositories/StatusRepo.java
+++ Cr-8/src/main/java/com/Cr_8/repositories/StatusRepo.java
@@ -1,6 +1,6 @@
 package com.Cr_8.repositories;
 
-import java.util.Optional;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -15,6 +15,6 @@ public interface StatusRepo extends JpaRepository<Status,Integer> {
      * @param id the id of the status to be found
      * @return an Optional containing the Status object if found, or an empty Optional if not found
      */
-	Optional<Status> findById(Integer id);
+	Status findById(int id);
 	
 }
