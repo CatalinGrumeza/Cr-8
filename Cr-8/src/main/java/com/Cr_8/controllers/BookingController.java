e4ec7715cc1a64a10dfd6f284d38d49a88caa8cf
Catalin Grumeza <catalin.grumeza@edu.itspiemonte.it>
2025-01-14 15:12:10 +0100

    to lower case mail

--- Cr-8/src/main/java/com/Cr_8/controllers/BookingController.java
+++ Cr-8/src/main/java/com/Cr_8/controllers/BookingController.java
@@ -68,7 +68,7 @@ public class BookingController {
 		if(result.hasErrors()) {
 			return new ResponseEntity<>(errore(result).toString(), HttpStatus.BAD_REQUEST);
 		} else {
-		    bookService.createBooking(bookRequest.getDataFrom(),bookRequest.getDataTo(),bookRequest.getAdditionalDetails(),bookRequest.getParticipantNumber(),bookRequest.getBookType(),bookRequest.getVisitorType(),bookRequest.getName(),bookRequest.getSurname(),bookRequest.getPhone(),bookRequest.getLabs(),bookRequest.getEmail(),bookRequest.getNumberOfDays());
+		    bookService.createBooking(bookRequest.getDataFrom(),bookRequest.getDataTo(),bookRequest.getAdditionalDetails(),bookRequest.getParticipantNumber(),bookRequest.getBookType(),bookRequest.getVisitorType(),bookRequest.getName(),bookRequest.getSurname(),bookRequest.getPhone(),bookRequest.getLabs(),bookRequest.getEmail().toLowerCase(),bookRequest.getNumberOfDays());
 		    mailService.sendEmail(bookRequest.getEmail(), bookRequest.getAdditionalDetails(), "Richiesta di Prenotazione", bookRequest.getName(), bookRequest.getSurname(), "Prenotazione");
 		    mailService.sendEmailToAdmin(bookRequest.getEmail(), bookRequest.getAdditionalDetails(), "Richiesta di Prenotazione", bookRequest.getName(), bookRequest.getSurname(),"Prenotazione",bookRequest.getPhone());
 		    return ResponseEntity.ok("Book request created successfully!");
