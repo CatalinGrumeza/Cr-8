c20fc681ffe08a83377dd115b913be6be1b0e90c
ggiaco-lab <alessio.giacometti@edu.itspiemonte.it>
2025-01-21 16:59:26 +0100

    Created delete labs front, fixed RemoveExistLabs service (#125)
    Co-authored-by: CatalinGrumeza <catalin.grumeza@edu.itspiemonte.it>

--- Cr-8/src/main/java/com/Cr_8/config/WebConfig.java
+++ Cr-8/src/main/java/com/Cr_8/config/WebConfig.java
@@ -16,8 +16,9 @@ public class WebConfig implements WebMvcConfigurer{
         registry.addViewController("/dashboard/all-info").setViewName("forward:/backdoor/all-info.html");
         registry.addViewController("/dashboard/all-admin").setViewName("forward:/backdoor/all-admin.html");
         registry.addViewController("/dashboard").setViewName("forward:/backdoor/dashboard.html");
+        registry.addViewController("/dashboard/delete-labs").setViewName("forward:/backdoor/delete-labs.html");
         registry.addViewController("/dashboard/add-labs").setViewName("forward:/backdoor/add-labs.html");
-//        registry.addRedirectViewController("/login", "/login.html");
+
 
     }
 }
