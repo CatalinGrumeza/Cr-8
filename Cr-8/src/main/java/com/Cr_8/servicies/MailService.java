e83880c6f6e3ed0831fc3122611e83fc50ee1703
CatalinGrumeza <catalin.grumeza@edu.itspiemonte.it>
2025-01-21 17:55:36 +0100

    email format (#139)

--- Cr-8/src/main/java/com/Cr_8/servicies/MailService.java
+++ Cr-8/src/main/java/com/Cr_8/servicies/MailService.java
@@ -42,28 +42,78 @@ public class MailService {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setFrom(fromEmailId);
         simpleMailMessage.setTo(bookRequest.getEmail());
-        simpleMailMessage.setText(
-            String.format(
-                "Gentile %s %s, abbiamo ricevuto la sua richiesta di %s.\n\n Di seguito i dettagli da lei inviati:\n- Visitatore:%s\n- Numero Partecipanti:%s\n- Durata soggiorno richiesta:%s \n\nCordiali saluti,\nLo staff di Educaccia",
-                bookRequest.getName(), bookRequest.getSurname(),type,bookRequest.getVisitorType(),bookRequest.getParticipantNumber(),bookRequest.getNumberOfDays()
-            )
-        );
-        simpleMailMessage.setSubject("Richiesta prenotazione");
-        javaMailSender.send(simpleMailMessage);
+        String emailBody = String.format(
+                "Gentile %s %s,\n\n" +
+                "La ringraziamo per averci contattato e per aver scelto Cascina Caccia. Siamo lieti di confermare che abbiamo ricevuto la sua richiesta di prenotazione relativa a %s.\n\n" +
+                "Di seguito, i dettagli forniti nella sua richiesta:\n\n" +
+                "- Tipo di Visitatore: %s\n" +
+                "- Numero di Partecipanti: %d\n" +
+                "- Data di Inizio: %s\n" +
+                "- Data di Fine: %s\n" +
+                "- Durata del Soggiorno: %d giorni\n" +
+                "- Laboratori Selezionati: %s\n" +
+                "- Dettagli Aggiuntivi: %s\n\n" +
+                "Il nostro staff procederà con la revisione della sua richiesta e sarà presto in contatto per fornirle ulteriori informazioni o per confermare la prenotazione.\n\n" +
+                "Per qualsiasi necessità, non esiti a contattarci all'indirizzo email %s o al numero %s.\n\n" +
+                "Cordiali saluti,\n" +
+                "Lo Staff di Cascina Caccia\n\n" +
+                "Via Serra Alta, 6, 10020 San Sebastiano da Po TO\n" +
+                "+39 348 642 5023\n" +
+                "cascina.caccia@acmos.net\n" +
+                "https://cascinacaccia.net/",
+                bookRequest.getName(),
+                bookRequest.getSurname(),
+                type,
+                bookRequest.getVisitorType(),
+                bookRequest.getParticipantNumber(),
+                bookRequest.getDataFrom() != null ? bookRequest.getDataFrom().toString() : "Non specificata",
+                bookRequest.getDataTo() != null ? bookRequest.getDataTo().toString() : "Non specificata",
+                bookRequest.getNumberOfDays(),
+                bookRequest.getLabs() != null ? String.join(", ", bookRequest.getLabs()) : "Nessuno",
+                bookRequest.getAdditionalDetails() != null ? bookRequest.getAdditionalDetails() : "Nessuno",
+                "cascina.caccia@acmos.net",
+                "+39 348 642 5023" // Replace with actual contact number
+            );
+
+            simpleMailMessage.setText(emailBody);
+            simpleMailMessage.setSubject("Conferma ricezione richiesta di prenotazione");
+            javaMailSender.send(simpleMailMessage);
     }
-    public void sendEmailConfirmBooked(BookingRequest bookingRequest,LocalDate data) {
+    public void sendEmailConfirmBooked(BookingRequest bookingRequest) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setFrom(fromEmailId);
         simpleMailMessage.setTo(bookingRequest.getReference().getEmail());
-        simpleMailMessage.setText(
-            String.format(
-                "Gentile %s %s ,la sua richiesta di prenotazione è stata confermata con successo in data \n\n%s\n\nCordiali saluti,\nLo staff di Educaccia",
-                bookingRequest.getReference().getLastName(),bookingRequest.getReference().getFirstName(),data
-            )
+        String fraction="";
+        if(bookingRequest.getBookedDate().getDayFractions().isFullDay()) {
+        	fraction="Tutto il giorno";
+        }else if (bookingRequest.getBookedDate().getDayFractions().isMorning()) {
+        	fraction="Mattina";
+		}
+        String emailBody = String.format(
+            "Gentile %s %s,\n\n" +
+            "Siamo lieti di confermare che la sua richiesta di prenotazione è stata accettata con successo. Di seguito i dettagli della sua prenotazione:\n\n" +
+            "- Ore inizio: %s"+
+            "- Data di Inizio: %s\n" +
+            "- Data di Fine: %s\n\n" +
+            "La ringraziamo per aver scelto Cascina Caccia e restiamo a disposizione per qualsiasi necessità.\n\n" +
+            "Cordiali saluti,\n" +
+            "Lo Staff di Cascina Caccia\n\n" +
+            "Via Serra Alta, 6, 10020 San Sebastiano da Po TO\n" +
+            "+39 348 642 5023\n" +
+            "cascina.caccia@acmos.net\n" +
+            "cascinacaccia.net/",
+            bookingRequest.getReference().getFirstName(),
+            bookingRequest.getReference().getLastName(),
+            fraction,
+            bookingRequest.getBookedDate().getDate() != null ? bookingRequest.getBookedDate().getDate().toString() : "Non specificata",
+            		bookingRequest.getBookedDate().getToDate() != null ? bookingRequest.getBookedDate().getToDate().toString() : "Non specificata"
         );
+
+        simpleMailMessage.setText(emailBody);
         simpleMailMessage.setSubject("Prenotazione Confermata");
         javaMailSender.send(simpleMailMessage);
     }
+
     public void sendEmailCancelledBooked(BookingRequest bookingRequest) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setFrom(fromEmailId);
